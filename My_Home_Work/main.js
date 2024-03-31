e0b7f667bceea0f0559da5874f18e685f89a30ce
“ziaeezohreh909” <ziaeezohreh909@gmail.com>
2024-03-31 13:23:23 +0330

    install vite and tailwind

---
+++ My_Home_Work/main.js
@@ -0,0 +1,13 @@
+import "./src/index.css";
+
+document.querySelector("#app").innerHTML = `
+  <div>
+    <h1 class="bg-green-700">Hello Vite!</h1>
+    <div class="card">
+      <button id="counter" type="button"></button>
+    </div>
+    <p class="read-the-docs">
+      Click on the Vite logo to learn more
+    </p>
+  </div>
+`;
